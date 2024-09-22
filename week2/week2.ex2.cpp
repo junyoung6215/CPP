416c8c02f452c088ddfeea1c57e4b4af1df88ca0
junyoung6215 <junyoung6215@gmail.com>
2024-09-22 20:20:57 +0900

    2주차

---
+++ week2/week2.ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int number,sum,count=0;
+	cout<<"Enter 4 negative numbers:\n";
+
+	while(++count<=4)
+	{cin>> number;
+	if(number>=0){
+		cout<<"Error:Positive number"<<
+		"or zero entered\n";
+	}
+	
+	}
+}
